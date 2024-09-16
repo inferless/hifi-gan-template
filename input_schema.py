35d59cc76a9247ab994ac6aa28dafcb06b5b4777
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2024-09-17 03:36:00 +0530

    Create input_schema.py

---
+++ input_schema.py
@@ -0,0 +1,8 @@
+INPUT_SCHEMA = {
+  "audiofile_name":
+    {
+      "datatype": "BYTES",
+      "shape": [ 1 ],
+      "example": [ "https://inferless-public.s3.amazonaws.com/ss_gt_1.wav"  ]
+    } 
+}
